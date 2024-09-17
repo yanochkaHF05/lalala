1c2eca1e16cbc56e458c0e4d34d436f51b7df947
mka <mka@A34PC9>
2024-09-17 11:41:30 +0300

    degree

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,4 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
 
-Console.WriteLine("TRYLALA!");
+//Console.WriteLine("TRYLALA!");
+
+Console.WriteLine("-----------------weather -----------------");
+Console.Write("Enter curent degree: ");
+int degree = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"curent {degree * 1.8 + 32} degree!");
